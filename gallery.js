6f5f7f1b853671f0a47a94b6aaae9f250d3bf868
AllenOu <allenseac@gmail.com>
2017-07-22 22:39:56 +0800

    Image support double click zoom

--- gallery.js
+++ gallery.js
@@ -1,24 +1,65 @@
-function Gallery(elment, customOpts) {
-    // this.customOpts = customOpts
-    return new Gallery.fn.init(elment, customOpts)
+function Gallery(element, customOpts) {
+
+    return new Gallery.fn.init(element, customOpts)
 }
+
+
 Gallery.fn = {
 
-    init: function(elment, opts) {
-        var container = document.getElementById(elment),
+    init: function(element, opts) {
+        var container = document.getElementById(element),
             wrapper = container.firstElementChild,
             slides = document.getElementsByClassName('gallery-slide'),
             sw = window.screen.width
         wrapper.style.width = slides.length * sw + 'px'
 
+        this.customOpts = opts
+        this.slides = slides
 
+        this.pagination()
+        this.events()
+
+    },
+    pagination: function() {
         //HANDLE : pagination
-
+        var opts = this.customOpts
         var pagination = document.querySelector(opts['pagination'])
+        var slides = this.slides
         for (var i = 0; i < slides.length; i++) {
             bullet = document.createElement('span')
             bullet.className = 'gallery-pagination-bullet'
             pagination.appendChild(bullet)
         }
+    },
+    events: function() {
+        //HANDLE : events
+        if (!this.customOpts['zoom']) return
+
+        var hammerOpts = {
+
+        }
+        var container = document.getElementById('gallery-container')
+        var hammertime = new Hammer(container, hammerOpts);
+        var _this = this
+
+        hammertime.on('tap', function(e) {
+            console.log(e)
+            var element = e.target
+            if (element.nodeName === 'IMG' && e.tapCount === 2) {
+                if (element.getAttribute('style')) {
+                    element.removeAttribute('style');
+                    return
+                }
+                _this.zoom(e.target)
+            }
+        });
+    },
+    zoom: function(element) {
+        var sh = window.screen.height,
+            eh = element.clientHeight,
+            scale = sh / eh
+
+        element.style.webkitTransform = `scale(${scale})`
     }
 }
+Gallery.fn.init.prototype = Gallery.fn
