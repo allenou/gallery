4c9490d147cb288f5e7a220ea08f8696eb3431e8
AllenOu <allenseac@gmail.com>
2017-07-22 21:50:28 +0800

    Add basic layout

--- gallery.js
+++ gallery.js
@@ -1,9 +1,24 @@
-var Gallery = {
-    initGalleryWidth: function () {
-        var galleryWrapper = document.getElementById('gallery-wrapper')
-        var gallerySlides = document.getElementsByClassName('gallery-slide')
-        var screenWidth = window.screen.width
-        galleryWrapper.style.width = gallerySlides.length * screenWidth + 'px';
+function Gallery(elment, customOpts) {
+    // this.customOpts = customOpts
+    return new Gallery.fn.init(elment, customOpts)
+}
+Gallery.fn = {
+
+    init: function(elment, opts) {
+        var container = document.getElementById(elment),
+            wrapper = container.firstElementChild,
+            slides = document.getElementsByClassName('gallery-slide'),
+            sw = window.screen.width
+        wrapper.style.width = slides.length * sw + 'px'
+
+
+        //HANDLE : pagination
+
+        var pagination = document.querySelector(opts['pagination'])
+        for (var i = 0; i < slides.length; i++) {
+            bullet = document.createElement('span')
+            bullet.className = 'gallery-pagination-bullet'
+            pagination.appendChild(bullet)
+        }
     }
 }
-Gallery.initGalleryWidth()
