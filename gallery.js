08c1086190645388046fe0fab0be932e79ab3470
AllenOu <allenseac@gmail.com>
2017-07-23 16:15:11 +0800

    Optimized code

--- gallery.js
+++ gallery.js
@@ -1,7 +1,11 @@
+/*!
+ * Gallery
+ * (c) Allen Ou
+ */
+
 function Gallery(element, customOpts) {
     return new Gallery.fn.init(element, customOpts)
 }
-
 
 Gallery.fn = {
 
@@ -22,12 +26,19 @@ Gallery.fn = {
 
         this.spot()
         this.events()
+        this.slide()
+        this.zoom()
     },
     spot: function() {
         //HANDLE : pagination
-        var pagination = this.pagination
-        var slides = this.slides
-        for (var i = 0; i < slides.length; i++) {
+        if (!this.customOpts['pagination']) return
+
+        var slides = this.slides,
+            pagination = this.pagination,
+            i = 0,
+            length = slides.length
+
+        for (; i < length; i++) {
             bullet = document.createElement('span')
             bullet.className = 'gallery-pagination-bullet'
             bullet.setAttribute('data-index', i)
@@ -41,59 +52,28 @@ Gallery.fn = {
         var _this = this,
             wrapper = this.wrapper,
             sw = this.sw
-        if (!this.customOpts['zoom']) return
 
-        var hammerOpts = {
-
-        }
-
-        var gallery = new Hammer(wrapper, hammerOpts);
-
-        //HANDLE: picture zoom
-        gallery.on('tap', function(e) {
-            var element = e.target
-            if (element.nodeName === 'IMG' && e.tapCount === 2) {
-                if (element.getAttribute('style')) {
-                    element.removeAttribute('style');
-                    return
-                }
-                _this.zoom(e.target, e.center.x)
-            }
-        });
+        var gallery = new Hammer(wrapper)
 
         //HANDLE:picture pan
-        var type
-
         gallery.on('panmove', function(e) {
             var distance
+
             distance = -(sw * _this.currentIndex) + e.deltaX
-            wrapper.style.marginLeft = `${distance}px`;
+            wrapper.style.marginLeft = `${distance}px`
         })
 
         gallery.on('panend', function(e) {
-            console.log(e)
+
             var element = e.target,
                 mouseX = Math.abs(e.deltaX)
-            type = e.additionalEvent // event type
 
             if (mouseX < sw / 3) { //reductive translation
-                wrapper.style.marginLeft = `${-sw * _this.currentIndex}px`;
-                return;
+                wrapper.style.marginLeft = `${-sw * _this.currentIndex}px`
+                return
             }
             _this.slide(e.deltaX)
-        });
-
-        //HANDLE:pagination tap
-        var pagination = new Hammer(_this.pagination, {});
-        pagination.on('tap', function(e) {
-            var element = e.target,
-                index = element.getAttribute('data-index')
-            type = e.additionalEvent
-            if (index) {
-                _this.currentIndex = index
-                _this.slide()
-            }
-        });
+        })
     },
 
     slide: function(deltaX) {
@@ -103,6 +83,21 @@ Gallery.fn = {
             pagination = this.pagination,
             currentIndex = this.currentIndex,
             slidesLength = this.slidesLength
+
+        //HANDLE:pagination tap
+        if (this.customOpts['paginationClickable']) {
+            var paginationtime = new Hammer(_this.pagination)
+
+            paginationtime.on('tap', function(e) {
+                var element = e.target,
+                    index = element.getAttribute('data-index')
+
+                if (index) {
+                    _this.currentIndex = index
+                    _this.slide()
+                }
+            })
+        }
 
         if (deltaX < 0) {
             nextSlide()
@@ -114,17 +109,17 @@ Gallery.fn = {
 
         function prevSlide() {
             if (currentIndex > 0) {
-                _this.currentIndex = currentIndex -= 1;
-                slideTo(currentIndex);
+                _this.currentIndex = currentIndex -= 1
+                slideTo(currentIndex)
             } else {
-                wrapper.style.marginLeft = `${-sw * _this.currentIndex}px`;
+                wrapper.style.marginLeft = `${-sw * _this.currentIndex}px`
             }
         }
 
         function nextSlide() {
             if (currentIndex < slidesLength - 1) {
-                _this.currentIndex = currentIndex += 1;
-                slideTo(currentIndex);
+                _this.currentIndex = currentIndex += 1
+                slideTo(currentIndex)
             } else { // last picture
                 wrapper.style.marginLeft = `${-sw *  currentIndex}px`
             }
@@ -134,6 +129,7 @@ Gallery.fn = {
             var bullets = pagination.childNodes,
                 i = 0,
                 length = bullets.length
+
             for (; i < length; i++) {
                 if (currentIndex == i) {
                     bullets[i].className += ' gallery-pagination-clickable'
@@ -144,29 +140,47 @@ Gallery.fn = {
             wrapper.style.marginLeft = `-${sw * currentIndex}px`
         }
     },
-    zoom: function(element, mouseX) {
-        var sw = this.sw,
-            sh = window.screen.height,
-            ew = element.clientWidth,
-            eh = element.clientHeight,
-            scale = sh / eh,
-            fw = sw * scale - sw, //finally width
-            ot = sw / 3, //one third of the screen width
-            tx
+    zoom: function() {
+        //HANDLE: picture zoom
+        if (!this.customOpts['zoom']) return
 
-        /**
-         * Image zoom according to click direction
-         */
-        if (0 < mouseX && mouseX < ot) { //left
-            tx = fw / 2;
-        } else if (sw > ew || ot < mouseX && mouseX < sw / 2) { //center
-            // element.parentNode.style.width = fw
-            element.style.webkitTransform = `scale(${scale})`
-            return;
-        } else { //right
-            tx = -(fw / 2);
-        }
-        element.style.webkitTransform = `translate3d(${tx}px, 0px, 0px) scale(${scale})`;
+        var _this = this,
+            gallery = new Hammer(_this.wrapper)
+
+        gallery.on('tap', function(e) {
+            var element = e.target
+
+            if (element.nodeName === 'IMG' && e.tapCount === 2) {
+                var mouseX = Math.abs(e.center.x)
+
+                if (element.getAttribute('style')) {
+                    element.removeAttribute('style')
+                    return
+                }
+                var sw = _this.sw,
+                    sh = window.screen.height,
+                    ew = element.clientWidth,
+                    eh = element.clientHeight,
+                    scale = sh / eh,
+                    fw = sw * scale - sw, //finally width
+                    ot = sw / 3, //one third of the screen width
+                    tx
+
+                /**
+                 * Image zoom according to click direction
+                 */
+                if (0 < mouseX && mouseX < ot) { //left
+                    tx = fw / 2
+                } else if (sw > ew || ot < mouseX && mouseX < sw / 2) { //center
+                    // element.parentNode.style.width = fw
+                    element.style.webkitTransform = `scale(${scale})`
+                    return
+                } else { //right
+                    tx = -(fw / 2)
+                }
+                element.style.webkitTransform = `translate3d(${tx}px, 0px, 0px) scale(${scale})`
+            }
+        })
     }
 }
 Gallery.fn.init.prototype = Gallery.fn
